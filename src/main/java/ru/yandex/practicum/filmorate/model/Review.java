0c231ecd11bc4751b45a89a35f0aed665d8219fa
Kirill <kirill-bulanov@yandex.ru>
2023-03-01 22:17:19 +0500

    test

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+public class Review {
+    private Integer id;
+    @NonNull
+    @NotBlank
+    @Size(max = 2000)
+    private String content;
+    private Boolean isPositive;
+    private Integer userId;
+    private Integer filmId;
+    private Integer useful;
+}
